28a5517e3742bede71e0c199b37a99ee4048a6d2
ahmetucar1 <213214006@stu.adu.edu.tr>
2024-12-21 21:36:19 +0300

    İlk proje: Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+ namespace  HelloWorld
+ {
+    class Program 
+    {
+        static void Main(String[]args)
+        {
+            Console.WriteLine("Ahmet Uçar, Patika Bootcampinde olmamın sebebi yazılımcı olup, kariyerime ilk adımı güvenle atmak.");
+        }
+    }
+ }
